ac142b607b3c32a22180d5b8d2215adde320750b
CeronMoa <56114106+ceron999@users.noreply.github.com>
2024-05-29 09:37:19 +0900

    Eclipse commit

---
+++ BookSystem/src/Book.java
@@ -0,0 +1,15 @@
+
+public class Book {
+	public int bookIndex;
+	public String bookName;
+	public String bookAuthor;
+	public int bookPublicationYear;
+	
+	public Book(int getIndex, String getName, String getAuthor, int getYear)
+	{
+		bookIndex = getIndex;
+		bookName = getName;
+		bookAuthor = getAuthor;
+		bookPublicationYear = getYear;
+	}
+}
